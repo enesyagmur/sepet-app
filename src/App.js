cad02018d043eeb7ccbe5b6a093527ec2fea8f9b
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-04 19:27:44 +0300

    json verileri ile product componentini oluşturduk

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import products from "./products.json";
 import Header from "./components/Header";
-import Basket from "./components/Basket";
+// import Basket from "./components/Basket";
 import Product from "./components/Product";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="productContainer">
+        <div className="productBoxs">
+          {products.map((product) => (
+            <Product product={product} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-export default function Product() {
+export default function Product(props) {
   return (
-    <div>
-      
+    <div className="product">
+      <img src={props.product.img} />
+      <div className="productsInfo">
+        <p>{props.product.name}</p>
+        <p>{props.product.price}$</p>
+      </div>
+      <div className="countProduct">
+        <button>-</button>
+        <b>0</b>
+        <button>+</button>
+      </div>
     </div>
-  )
+  );
 }
