4abf531acb2b63642253eedb2e149692486dc9c7
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-04 18:54:51 +0300

    componentleri ve json dosyasını app.js e ekledik

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import products from "./products.json";
+import Header from "./components/Header";
+import Basket from "./components/Basket";
+import Product from "./components/Product";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
